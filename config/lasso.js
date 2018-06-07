190c41c29baf726281428a9dc4b3fd2401a654a4
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-06-07 21:51:13 +0530

    updates

---
+++ config/lasso.js
@@ -0,0 +1,10 @@
+const lassoConfig = {
+    plugins: [
+        'lasso-marko' // Allow Marko templates to be compiled and transported to the browser
+    ],
+    outputDir: 'static', // Place all generated JS/CSS/etc. files into the "static" dir
+    bundlingEnabled: false, // Only enable bundling in production
+    minify: false, // Only minify JS and CSS code in production
+    fingerprintsEnabled: false, // Only add fingerprints to URLs in production
+};
+module.exports = lassoConfig;
