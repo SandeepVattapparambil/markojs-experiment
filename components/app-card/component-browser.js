a7bd354eb156c25e277b4fed1f3f13a1cdb2a5d6
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-06-08 20:40:46 +0530

    updates

---
+++ components/app-card/component-browser.js
@@ -0,0 +1,9 @@
+/**
+ * Split component class for browser
+ */
+class AppCardClient {
+    onMount(input){
+        console.log('component mounted');
+    }
+}
+module.exports = AppCardClient;
---
+++ components/app-card/component.js
@@ -0,0 +1,15 @@
+/**
+ * Split component class for server
+ */
+class AppCardServer {
+    onCreate(input) {
+        let self = this;
+        if (input == null || !input || input == undefined) {
+            throw new Error('Component cannot be initialized without an input');
+        }
+        self.state = {
+            imageData: input
+        };
+    }
+}
+module.exports = AppCardServer;
