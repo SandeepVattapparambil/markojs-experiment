c40298ceadec819011e5c215baaa7e9f2583b01d
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-06-09 15:56:15 +0530

    updates

---
+++ components/app-main/component-browser.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+const URLS = require('../../config/api');
+class AppMain {
+    onMount() {
+        let start = 1;
+        let end = 9;
+        axios.get(URLS.BASE_API_URL + '/list/' + start + '/' + end)
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+}
+module.exports = AppMain;
